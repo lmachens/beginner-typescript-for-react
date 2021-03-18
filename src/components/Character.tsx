ac8fb9e5ded9dcc6d5564b1ece500ed01521fe15
lmachens <leon@machens.koeln>
2021-03-18 17:10:04 +0100

    ✨ Add status component

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,14 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+
+const statusToColor = {
+  Alive: "green",
+  Dead: "red",
+  unknown: "darkgray",
+};
+
+type StatusProps = {
+  status: "Alive" | "Dead" | "unknown";
+};
+const Status = styled.div<StatusProps>`
+  width: 30px;
+  height: 30px;
+  background: ${(props) => statusToColor[props.status]};
+  border-radius: 50%;
+`;
 
 type CharacterProps = {
   name: string;
   image: string;
+  status: "Alive" | "Dead" | "unknown";
 };
-function Character({ name, image }: CharacterProps) {
+function Character({ name, image, status }: CharacterProps) {
   return (
     <article>
       <h3>{name}</h3>
       <img src={image} alt="" />
+      <Status status={status} />
     </article>
   );
 }
